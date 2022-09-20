c35b801fd3888892277d866e0395cbe52714805e
tonythayers <35508022+tonythayers@users.noreply.github.com>
2022-09-20 15:58:13 +0100

    Update streamlit_app.py
    Added S3 data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,12 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('綾 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' Kale, Spinach & Rocket Smoothie')
 streamlit.text('塞 Hard-Boiled Free Range Egg')
 streamlit.text('陋 Avacado Toast')
 
 streamlit.header('Build Your Own Smoothie')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
