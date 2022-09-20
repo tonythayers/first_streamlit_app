1218808d4c50c811496930a103e65a3e587d9272
tonythayers <35508022+tonythayers@users.noreply.github.com>
2022-09-20 15:06:40 +0100

    Update streamlit_app.py
    Test emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(' 北 Omega 3 & Blueberry Oatmeal')
+streamlit.text(北'Omega 3 & Blueberry Oatmeal')
 streamlit.text('綾 Kale, Spinach & Rocket Smoothie')
 streamlit.text('塞 Hard-Boiled Free Range Egg')
 streamlit.text('陋 Avacado Toast')
 
-streamlit.header('Build Your Own Smoothie')
+streamlit.header('Build Your Own Smoothie')✅
