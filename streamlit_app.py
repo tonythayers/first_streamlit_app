5d17bab9c359e024a0ba37191bc767a53cd2ad74
tonythayers <35508022+tonythayers@users.noreply.github.com>
2022-09-20 15:07:36 +0100

    Update streamlit_app.py
    Test 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(北'Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('綾 Kale, Spinach & Rocket Smoothie')
 streamlit.text('塞 Hard-Boiled Free Range Egg')
 streamlit.text('陋 Avacado Toast')
