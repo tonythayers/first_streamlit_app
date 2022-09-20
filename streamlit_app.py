83c743d30584acfc33863cc841053d7199bba0f9
tonythayers <35508022+tonythayers@users.noreply.github.com>
2022-09-20 20:32:57 +0100

    Update streamlit_app.py
    Updated for emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text(' Kale, Spinach & Rocket Smoothie')
-streamlit.text('塞 Hard-Boiled Free Range Egg')
-streamlit.text('陋 Avacado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥦 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑 Avacado Toast')
 
-streamlit.header('Build Your Own Smoothie')
+streamlit.header('🍌🍎Build Your Own Smoothie🫐🍍')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
