8b56a52099da65bbe27a61fa341830d28b63e5c3
tonythayers <35508022+tonythayers@users.noreply.github.com>
2022-09-22 11:37:10 +0100

    Update streamlit_app.py
    Moved stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
-
+streamlit.stop()
 #import snowflake.connector
 #my_cur = my_cnx.cursor()
 #streamlit.header("The fruit load list contains:")
